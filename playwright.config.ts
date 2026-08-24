2c3685589867eab91618751189606c55deaa4662
berktutu <brktutu@gmail.com>
2026-08-24 15:51:07 +0300

    Configured playwright config (screenshots)

--- playwright.config.ts
+++ playwright.config.ts
@@ -14,7 +14,8 @@ export default defineConfig({
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [["html"], ["github"]],
   use: {
-    trace: "on-first-retry",
+    trace: "retain-on-failure",
+    screenshot: "only-on-failure",
   },
 
   projects: [
